627be96e2cbb8026a00061a90f6dfc700b9f1cee
nguyen long <longnguyenhoang26@gmail.com>
2020-11-17 13:44:47 +0700

    commit

--- js/bai10_listProduct.js
+++ js/bai10_listProduct.js
@@ -1,7 +1,7 @@
 function listProduct(count) {
     var list = [];
     for (var i = 0; i < count; i++) {
-        var listPro = new Product(i, "Product" + i, 100 + i, new Date().toLocaleDateString(), i + 1, i%2);
+        var listPro = new Product(i, "Long " + i, 100 + i, new Date().toLocaleDateString(), i + 1, i%2);
         list.push(listPro);
     }
     return list;
